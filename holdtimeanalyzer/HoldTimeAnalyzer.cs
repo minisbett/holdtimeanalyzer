5e0844da1b0ff4337904d04bb7e2e40770c3f694
minisbett <39670899+minisbett@users.noreply.github.com>
2024-06-20 15:36:03 +0200

    Projektdateien hinzufügen.

---
+++ holdtimeanalyzer/HoldTimeAnalyzer.cs
@@ -0,0 +1,75 @@
+﻿using OsuParsers.Enums.Replays;
+using OsuParsers.Replays;
+using HoldTimes = System.Collections.Generic.Dictionary<int, int>;
+
+namespace holdtimeanalyzer;
+
+/// <summary>
+/// Calculates the hold times for replays.
+/// </summary>
+internal static class HoldTimeAnalyzer
+{
+  /// <summary>
+  /// Calculates the hold times for the given replay, for both keys (K1 & M1, K2 & M2).
+  /// </summary>
+  /// <param name="replay">The replay to calculate the hold time of.</param>
+  /// <returns>The hold times per-key.</returns>
+  public static (HoldTimes Key1, HoldTimes Key2) CalculateHoldTimes(Replay replay)
+  {
+    // Extract key-specific frames for both keys.
+    KeyFrame[] key1Frames = ExtractFramesForKey(replay, StandardKeys.K1 & StandardKeys.M1);
+    KeyFrame[] key2Frames = ExtractFramesForKey(replay, StandardKeys.K2 & StandardKeys.M2);
+
+    // Calculate the hold times for both keys.
+    HoldTimes key1HoldTimes = CalculateHoldTimes(key1Frames);
+    HoldTimes key2HoldTimes = CalculateHoldTimes(key2Frames);
+
+    return (key1HoldTimes, key2HoldTimes);
+  }
+
+  /// <summary>
+  /// Returns the hold times for the given key-specific frames.
+  /// </summary>
+  /// <param name="frames">The key-specific frames.</param>
+  /// <returns>The hold times (key) and amount of occurences (.</returns>
+  private static HoldTimes CalculateHoldTimes(KeyFrame[] frames)
+  {
+    HoldTimes holdTimes = new HoldTimes();
+    KeyFrame? pressedFrame = null;
+
+    foreach (var frame in frames)
+    {
+      // If no pressed frame is set and the current frame is pressed, ´measure time from here.
+      if (pressedFrame is null && frame.Pressed)
+        pressedFrame = frame;
+      // If an unpress frame is encountered, measure the hold time via the pressed frame and increase the amount of occurences.
+      else if (pressedFrame is not null && !frame.Pressed)
+      {
+        int holdTime = frame.Offset - pressedFrame.Offset;
+        holdTimes.TryAdd(holdTime, 0); // Ensure the hold time is in the dictionary.
+        holdTimes[frame.Offset - pressedFrame.Offset]++;
+        pressedFrame = null; // reset the pressed frame again to measure the next hold time.
+      }
+    }
+
+    return holdTimes;
+  }
+
+  /// <summary>
+  /// Converts the replay frames in the specified replays into key-specific frames of the given key.
+  /// </summary>
+  /// <param name="replay">The replay containing the replay frames.</param>
+  /// <param name="key">The key to get the specific frames of.</param>
+  /// <returns>The key-specific frames.</returns>
+  private static KeyFrame[] ExtractFramesForKey(Replay replay, StandardKeys key)
+  {
+    return replay.ReplayFrames.Select(n => new KeyFrame(n.Time, n.StandardKeys.HasFlag(key))).ToArray();
+  }
+}
+
+/// <summary>
+/// Represents a frame in a replay, with the offset and whether the key this frame is related to was pressed.
+/// </summary>
+/// <param name="Offset">The offset of the frame from the start.</param>
+/// <param name="Pressed">Bool whether the key related to this frame is pressed.</param>
+internal record KeyFrame(int Offset, bool Pressed);
---
+++ holdtimeanalyzer/Program.cs
@@ -0,0 +1,121 @@
+﻿using holdtimeanalyzer;
+using OsuParsers.Decoders;
+using OsuParsers.Enums;
+using OsuParsers.Replays;
+using ScottPlot;
+using ScottPlot.Plottables;
+using System.Globalization;
+using HoldTimes = System.Collections.Generic.Dictionary<int, int>;
+
+public class Program
+{
+  private static int _current = 0;
+  private static int _total = 0;
+
+  public static void Main(string[] args)
+  {
+    Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+
+    // Ensure that a replays & output folder exists.
+    Directory.CreateDirectory("output");
+    if (!Directory.Exists("replays"))
+    {
+      Directory.CreateDirectory("replays");
+      Console.WriteLine("A replays folder was created. Put your replays in there and run the program again.");
+      Console.ReadKey();
+      return;
+    }
+
+    // go through all replay files in the replays folder and process them.
+    string[] replayFiles = Directory.GetFiles("replays", "*.osr");
+    _total = replayFiles.Length;
+    foreach (string replayFile in replayFiles)
+    {
+      try
+      {
+        _current++;
+        ProcessReplayFile(replayFile);
+      }
+      catch (Exception ex)
+      {
+        // If an error occurs, notify the user and write the error to a file.
+        Write("Error", $"{Path.GetFileName(replayFile)} => {ex.Message}", ConsoleColor.Red);
+        File.WriteAllText($"output\\error_{Path.GetFileNameWithoutExtension(replayFile)}.txt", $"{ex.Message}\n{ex.StackTrace}");
+      }
+    }
+
+    Console.WriteLine();
+    Console.WriteLine("Done.");
+    Console.ReadKey();
+  }
+
+  /// <summary>
+  /// Processes the specified replay file and generates a plot for the hold times.
+  /// </summary>
+  /// <param name="replayFile">The replay file.</param>
+  private static void ProcessReplayFile(string replayFile)
+  {
+    // Calculate the hold times for the replay and filter them to only include hold times below 100ms.
+    Replay replay = ReplayDecoder.Decode(replayFile);
+    (HoldTimes Key1, HoldTimes Key2) holdTimes = HoldTimeAnalyzer.CalculateHoldTimes(replay);
+    HoldTimes filteredKey1 = new HoldTimes(holdTimes.Key1.Where(x => x.Key <= 100));
+    HoldTimes filteredKey2 = new HoldTimes(holdTimes.Key2.Where(x => x.Key <= 100));
+
+    // If no significant hold times were found, skip the replay.
+    if (!filteredKey1.Any() && !filteredKey2.Any())
+    {
+      Write("Skipped", $"{Path.GetFileName(replayFile)} (no significant hold times found)", ConsoleColor.Yellow);
+      return;
+    }
+
+    // Setup the plot.
+    Plot plot = new Plot();
+    string mods = string.Join(", ", Enum.GetValues<Mods>().Where(m => replay.Mods.HasFlag(m) && m > Mods.None).Select(m => m.ToString()));
+    string id = $"{(replay.OnlineId == 0 ? "Offline" : replay.OnlineId)}";
+    plot.Title($"({id}) {replay.PlayerName} at {replay.ReplayTimestamp:dd/MM/yyyy HH:mm} +{mods} ({replay.ReplayLength}ms)");
+    plot.ScaleFactor = 2;
+    plot.Axes.Left.Label.Text = "Amount of Occurrences";
+    plot.Axes.Bottom.Label.Text = "Milliseconds";
+
+    plot.Legend.Alignment = Alignment.UpperRight;
+
+    // Configure the limits for the axes, depending on the bounds of the hold times.
+    plot.Axes.Bottom.Min = filteredKey1.Concat(filteredKey2).Min(x => x.Key);
+    plot.Axes.Bottom.Max = filteredKey1.Concat(filteredKey2).Max(x => x.Key) + 1;
+    plot.Axes.Left.Min = filteredKey1.Concat(filteredKey2).Min(x => x.Value);
+    plot.Axes.Left.Max = (int)(filteredKey1.Concat(filteredKey2).Max(x => x.Value) * 1.05) + 1;
+
+    // Add the exponential average lines for both keys.
+    double exponentialAvg1 = filteredKey1.Sum(x => x.Key * Math.Pow(x.Value, 2)) / filteredKey1.Sum(x => Math.Pow(x.Value, 2));
+    double exponentialAvg2 = filteredKey2.Sum(x => x.Key * Math.Pow(x.Value, 2)) / filteredKey2.Sum(x => Math.Pow(x.Value, 2));
+    plot.Add.VerticalLine(exponentialAvg1, color: new Color(121, 159, 203), pattern: LinePattern.Dotted);
+    plot.Add.VerticalLine(exponentialAvg2, color: new Color(249, 102, 94), pattern: LinePattern.Dotted);
+
+    // Add the bar plots for both keys.
+    BarPlot key1Bar = plot.Add.Bars(filteredKey1.Keys.Select(x => x * 1d), filteredKey1.Values.Select(x => x * 1d));
+    BarPlot key2Bar = plot.Add.Bars(filteredKey2.Keys.Select(x => x * 1d), filteredKey2.Values.Select(x => x * 1d));
+    key1Bar.Color = new Color(121, 159, 203, 100);
+    key2Bar.Color = new Color(249, 102, 94, 100);
+    key1Bar.LegendText = $"Key 1 ({filteredKey1.Sum(x => x.Value)} presses, {Math.Round(exponentialAvg1, 2)} eavg.)";
+    key2Bar.LegendText = $"Key 2 ({filteredKey2.Sum(x => x.Value)} presses, {Math.Round(exponentialAvg2, 2)} eavg.)";
+
+    // Render the plot and save it to the output folder.
+    plot.SavePng($"output\\{Path.GetFileNameWithoutExtension(replayFile)}.png", 2000, 1200);
+    string progress = $"({_current}/{_total} {(int)(_current * 100d / _total)}%)";
+    Write("Processed", $"{Path.GetFileName(replayFile)} => output\\{Path.GetFileNameWithoutExtension(replayFile)}.png {progress}", ConsoleColor.Green);
+  }
+
+  /// <summary>
+  /// Writes a message to the console with the specified prefix color.
+  /// </summary>
+  /// <param name="prefix">The prefix of the message.</param>
+  /// <param name="text">The text.</param>
+  /// <param name="color">The prefix color.</param>
+  private static void Write(string prefix, string text, ConsoleColor color)
+  {
+    Console.ForegroundColor = color;
+    Console.Write(prefix + " ");
+    Console.ForegroundColor = ConsoleColor.Gray;
+    Console.WriteLine(text);
+  }
+}
